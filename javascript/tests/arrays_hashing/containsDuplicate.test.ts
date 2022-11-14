b3924fa13c101462e83f6ff19d6e78b083442fb6
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-14 16:16:36 +0300

    #4 : contains duplicate

---
+++ javascript/tests/arrays_hashing/containsDuplicate.test.ts
@@ -0,0 +1,14 @@
+import { containsDuplicate } from './../../arrays_hashing/contains_duplicate';
+
+
+
+describe('contains duplicate', () => { 
+    it('test one',()=>{
+        expect(containsDuplicate([1,2,3,1])).toBe(true)
+    })
+
+     it("test two", () => {
+       expect(containsDuplicate([1, 2, 3, 4])).toBe(false);
+     });
+
+ })
