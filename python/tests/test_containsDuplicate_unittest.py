c38796ad0dd31672ea513d41550bf082d3a6802b
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-17 15:16:51 +0300

    add python solution for isSubsequence

--- python/tests/test_containsDuplicate_unittest.py
+++ python/tests/test_containsDuplicate_unittest.py
@@ -6,15 +6,13 @@ import unittest
 from arrays.contains_duplicate import Solution
 
 
-class TestContainsDuplicate(unittest.TestCase):
+class TestContainsDuplicate(unittest.TestCase,Solution):
 
     def test_one(self):
-        solution = Solution()
 
-        self.assertEqual(solution.containsDuplicate([1,2,3,1]), True, "Should be true")
+        self.assertEqual(self.containsDuplicate([1,2,3,1]), True, "Should be true")
 
     def test_two(self):
-        solution = Solution()
 
-        self.assertEqual(solution.containsDuplicate([1,2,3,4]), False, "Should be false")
+        self.assertEqual(self.containsDuplicate([1,2,3,4]), False, "Should be false")
 
