97a20edcc0f25364f24b2c0c5b648366ea2db4cf
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-24 16:11:29 +0300

    add python solution for word search

---
+++ python/tests/test_wordSearch_unittest.py
@@ -0,0 +1,18 @@
+
+
+import unittest
+
+
+from backtracking.word_search import Solution
+
+class TestWordSearch(unittest.TestCase,Solution):
+
+    def test_one(self):
+        self.assertEqual(self.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED"),True,"Should be true")
+
+    def test_two(self):
+        self.assertEqual(self.exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], [
+                         "A", "D", "E", "E"]], "SEE"), True, "Should be true")
+        
+    def test_three(self):
+        self.assertEqual(self.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCB"),False,"Should be false")
