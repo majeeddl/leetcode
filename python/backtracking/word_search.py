73f01166adadba4b917550a0a98fc589ed33861d
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-24 16:31:59 +0300

    check again

--- python/backtracking/word_search.py
+++ python/backtracking/word_search.py
@@ -2,45 +2,77 @@
 ### o ( n * m * dfs)
 ### dfs => 4 ^ n
 
+from collections import Counter
+from itertools import chain
+
+
 class Solution:
     def exist(self, board : list[list[str]] , word : str) -> bool:
 
-        ROWS, COLS = len( board), len(board[0])
+        # ROWS, COLS = len( board), len(board[0])
 
-        path = set();
+        # path = set();
 
-        def dfs(r,c,i):
+        # def dfs(r,c,i):
             
-            if i== len(word):
-                return True
+        #     if i== len(word):
+        #         return True
             
-            if ( 
-                    r < 0 or
-                    c < 0 or
-                    r >= ROWS or
-                    c >= COLS or
-                    word[i] != board[r][c] or
-                    (r,c) in path
-                ):
-                return False
+        #     if ( 
+        #             r < 0 or
+        #             c < 0 or
+        #             r >= ROWS or
+        #             c >= COLS or
+        #             word[i] != board[r][c] or
+        #             (r,c) in path
+        #         ):
+        #         return False
             
-            path.add((r,c))
+        #     path.add((r,c))
 
-            res = (
-                dfs(r+1,c,i+1) or
-                dfs(r-1,c,i+1) or
-                dfs(r,c+1,i+1) or
-                dfs(r,c-1,i+1) 
-            )
+        #     res = (
+        #         dfs(r+1,c,i+1) or
+        #         dfs(r-1,c,i+1) or
+        #         dfs(r,c+1,i+1) or
+        #         dfs(r,c-1,i+1) 
+        #     )
 
-            path.remove((r,c))
+        #     path.remove((r,c))
+
+        #     return res
+        
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if dfs(r,c,0): 
+        #             return True
+        
+        # return False
+
+        row, col = len(board), len(board[0])
+        R, C, seen = range(row), range(col), set()
+
+        def dfs(coord, i=0):
+            
+            if len(word) == i: return True
+            
+            r,c = coord
+
+            if (r not in R or c not in C or 
+                coord in seen            or 
+                board[r][c] != word[i]): return False
+            
+            seen.add(coord)
+
+            res = (dfs((r+1,c), i+1) or dfs((r,c+1), i+1) or
+                   dfs((r-1,c), i+1) or dfs((r,c-1), i+1))
+            
+            seen.remove(coord)
 
             return res
-        
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r,c,0): 
-                    return True
+        boardCt, wrdCt = Counter(chain(*board)), Counter(word)
+        if any (boardCt[ch] < wrdCt[ch] for ch in wrdCt): return False
+
+        if boardCt[word[0]] > boardCt[word[-1]]: word = word[::-1]
         
-        return False
+        return any(dfs((r, c))  for c in C for r in R)
--- python/tests/test_wordSearch_unittest.py
+++ python/tests/test_wordSearch_unittest.py
@@ -5,14 +5,21 @@ import unittest
 
 from backtracking.word_search import Solution
 
-class TestWordSearch(unittest.TestCase,Solution):
+
+class TestWordSearch(unittest.TestCase, Solution):
 
     def test_one(self):
-        self.assertEqual(self.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED"),True,"Should be true")
+        self.assertEqual(self.exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], [
+                         "A", "D", "E", "E"]], "ABCCED"), True, "Should be true")
 
     def test_two(self):
         self.assertEqual(self.exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], [
                          "A", "D", "E", "E"]], "SEE"), True, "Should be true")
-        
+
     def test_three(self):
-        self.assertEqual(self.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCB"),False,"Should be false")
+        self.assertEqual(self.exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], [
+                         "A", "D", "E", "E"]], "ABCB"), False, "Should be false")
+
+    def test_four(self):
+        self.assertEqual(self.exist([["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], [
+                         "A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"], ["A", "A", "A", "A", "A", "A"]], "AAAAAAAAAAAABAA"), False, "Should be false")
