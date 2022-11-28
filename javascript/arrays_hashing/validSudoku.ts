e10fc5bc7638da35ff423b05398b077538ca7a47
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-28 15:40:11 +0300

    fix  bug

--- javascript/arrays_hashing/validSudoku.ts
+++ javascript/arrays_hashing/validSudoku.ts
@@ -1,8 +1,8 @@
 export const isValidSudoku = (board: string[][]): boolean => {
-  const cols = {};
-  const rows = {};
+  // const cols = {};
+  // const rows = {};
 
-  const squares = {};
+  // const squares = {};
 
   const validRow = (board, row, col, value) => {
     for (let j = 0; j < 8; j++) {
@@ -17,19 +17,19 @@ export const isValidSudoku = (board: string[][]): boolean => {
   };
 
   const validColumn = (board, row, col, value) => {
-     for (let i = 0; i < 8; i++) {
-       // check if the current row matches the passed in row
-       if (i !== row) {
-         if (board[i][col] === value) {
-           return false;
-         }
-       }
-     }
+    for (let i = 0; i < 8; i++) {
+      // check if the current row matches the passed in row
+      if (i !== row) {
+        if (board[i][col] === value) {
+          return false;
+        }
+      }
+    }
 
-     return true;
+    return true;
   };
 
-  const validBox = (board, row, col, value)=> {
+  const validBox = (board, row, col, value) => {
     const startRow = row - (row % 3),
       startCol = col - (col % 3);
 
@@ -44,54 +44,52 @@ export const isValidSudoku = (board: string[][]): boolean => {
     }
 
     return true;
-  }
+  };
 
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
+      const value = board[i][j];
+      if (value !== ".") {
+        if (
+          !validRow(board, i, j, value) ||
+          !validColumn(board, i, j, value) || !validBox(board, i, j, value)
+        ) {
+          return false;
+        }
+      }
 
       // const value = board[i][j];
-      // if (value !== ".") {
-      //   if (
-      //     !validRow(board, i, j, value) ||
-      //     !validColumn(board, i, j, value) || !validBox(board, i, j, value)
-      //   ) {
-      //     return false;
-      //   }
+
+      // const q = (i % 3) + "_" + (j % 3);
+
+      // if (value == ".") {
+      //   continue;
       // }
 
-      const value = board[i][j];
+      // if (!rows[i]) {
+      //   rows[i] = [];
+      // }
 
-      const q = (i % 3) + "_" + (j % 3);
+      // if (!cols[j]) {
+      //   cols[j] = [];
+      // }
+      // if (!squares[q]) {
+      //   squares[q] = [];
+      // }
 
-      if (value == ".") {
-        continue;
-      }
+      // if (
+      //   rows[i].includes(value) ||
+      //   cols[j].includes(value) ||
+      //   squares[q].includes(value)
+      // ) {
+      //   return false;
+      // }
 
-      if (!rows[i]) {
-        rows[i] = [];
-      }
-
-      if (!cols[j]) {
-        cols[j] = [];
-      }
-      if (!squares[q]) {
-        squares[q] = [];
-      }
-
-      if (
-        rows[i].includes(value) ||
-        cols[j].includes(value) ||
-        squares[q].includes(value)
-      ) {
-        // return false;
-      }
-
-      rows[i].push(value);
-      cols[j].push(value);
-      squares[q].push(value);
+      // rows[i].push(value);
+      // cols[j].push(value);
+      // squares[q].push(value);
     }
   }
-
 
   return true;
 };
